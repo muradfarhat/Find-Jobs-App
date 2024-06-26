48a4d5387d4f0664822e9283ee0ebca6fc37e227
Murad Farhat <92052407+muradfarhat@users.noreply.github.com>
2024-06-26 13:54:47 +0400

    Add Fragments
    - Add User Card Fragment
    - Add Recycler View Card Fragment

---
+++ app/src/main/java/com/example/sampleproject/RecyclerViewCardFragment.kt
@@ -0,0 +1,147 @@
+package com.example.sampleproject
+
+import android.os.Bundle
+import android.app.Fragment
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sampleproject.interfaces.OnItemClickListener
+import com.example.sampleproject.views.activities.Item
+import com.example.sampleproject.views.activities.JobDetailsActivity
+import com.example.sampleproject.views.activities.JobItemAdapter
+import com.example.sampleproject.views.activities.RecyclerViewActivity
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [RecyclerViewCardFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class RecyclerViewCardFragment : Fragment(), OnItemClickListener {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    private val data = listOf(
+        Item(
+            "UI/UX Designer",
+            "Elux Space, Malang",
+            "Part-time",
+            "Remote",
+            "Junior level",
+            "Your profile match this job",
+            "Yesterday",
+            "\$300 - 500/month"
+        ),
+        Item(
+            "Product Designer",
+            "Flip, Jakarta Barat",
+            "Full-time",
+            "On-site",
+            "Senior level",
+            "Your profile match this job",
+            "Now",
+            "\$12k - 20k/years"
+        ),
+        Item(
+            "UI/UX Designer",
+            "Elux Space, Malang",
+            "Part-time",
+            "Remote",
+            "Junior level",
+            "Your profile match this job",
+            "Yesterday",
+            "\$300 - 500/month"
+        ),
+        Item(
+            "Product Designer",
+            "Flip, Jakarta Barat",
+            "Full-time",
+            "On-site",
+            "Senior level",
+            "Your profile match this job",
+            "Now",
+            "\$12k - 20k/years"
+        ),
+        Item(
+            "UI/UX Designer",
+            "Elux Space, Malang",
+            "Part-time",
+            "Remote",
+            "Junior level",
+            "Your profile match this job",
+            "Yesterday",
+            "\$300 - 500/month"
+        ),
+        Item(
+            "Product Designer",
+            "Flip, Jakarta Barat",
+            "Full-time",
+            "On-site",
+            "Senior level",
+            "Your profile match this job",
+            "Now",
+            "\$12k - 20k/years"
+        )
+    )
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_recycler_view_card, container, false)
+
+        recyclerView = view.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(RecyclerViewActivity())
+
+        val adapter = JobItemAdapter(data, this)
+        recyclerView.adapter = adapter
+
+        return view
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment RecyclerViewCardFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            RecyclerViewCardFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+
+    override fun onItemClick(position: Int) {
+        val intent = Intent(RecyclerViewActivity(), JobDetailsActivity::class.java)
+        intent.putExtra("tvCompany", data[position].tvCompany)
+        intent.putExtra("tvTittle", data[position].tvTitle)
+        intent.putExtra("salary", data[position].salary)
+        startActivity(intent)
+    }
+}
--- app/src/main/java/com/example/sampleproject/views/activities/RecyclerViewActivity.kt
+++ app/src/main/java/com/example/sampleproject/views/activities/RecyclerViewActivity.kt
@@ -8,98 +8,26 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sampleproject.R
+import com.example.sampleproject.RecyclerViewCardFragment
+import com.example.sampleproject.UserCardFragment
 import com.example.sampleproject.interfaces.OnItemClickListener
 
-class RecyclerViewActivity : AppCompatActivity(), OnItemClickListener {
-
-    private val data = listOf(
-        Item(
-            "UI/UX Designer",
-            "Elux Space, Malang",
-            "Part-time",
-            "Remote",
-            "Junior level",
-            "Your profile match this job",
-            "Yesterday",
-            "\$300 - 500/month"
-        ),
-        Item(
-            "Product Designer",
-            "Flip, Jakarta Barat",
-            "Full-time",
-            "On-site",
-            "Senior level",
-            "Your profile match this job",
-            "Now",
-            "\$12k - 20k/years"
-        ),
-        Item(
-            "UI/UX Designer",
-            "Elux Space, Malang",
-            "Part-time",
-            "Remote",
-            "Junior level",
-            "Your profile match this job",
-            "Yesterday",
-            "\$300 - 500/month"
-        ),
-        Item(
-            "Product Designer",
-            "Flip, Jakarta Barat",
-            "Full-time",
-            "On-site",
-            "Senior level",
-            "Your profile match this job",
-            "Now",
-            "\$12k - 20k/years"
-        ),
-        Item(
-            "UI/UX Designer",
-            "Elux Space, Malang",
-            "Part-time",
-            "Remote",
-            "Junior level",
-            "Your profile match this job",
-            "Yesterday",
-            "\$300 - 500/month"
-        ),
-        Item(
-            "Product Designer",
-            "Flip, Jakarta Barat",
-            "Full-time",
-            "On-site",
-            "Senior level",
-            "Your profile match this job",
-            "Now",
-            "\$12k - 20k/years"
-        )
-    )
-    private lateinit var username: String
-    private lateinit var usernameText: TextView
-    private lateinit var recyclerView: RecyclerView
+class RecyclerViewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_recycler_view)
 
-        username = intent.getStringExtra("EXTRA_USERNAME").toString()
-        usernameText = findViewById(R.id.usernameText)
-        usernameText.setText("Hi, $username")
-//        usernameText.text = "Hi, $username"
+        // Add User Card Fragment
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.userCard, UserCardFragment())
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
 
-        recyclerView = findViewById(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-
-        val adapter = JobItemAdapter(data, this)
-        recyclerView.adapter = adapter
-    }
-
-    override fun onItemClick(position: Int) {
-        val intent = Intent(this, JobDetailsActivity::class.java)
-        intent.putExtra("tvCompany", data[position].tvCompany)
-        intent.putExtra("tvTittle", data[position].tvTitle)
-        intent.putExtra("salary", data[position].salary)
-        startActivity(intent)
+        // Add Recycler View Card Fragment
+        fragmentTransaction.add(R.id.recyclerViewCard, RecyclerViewCardFragment())
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
     }
 }
