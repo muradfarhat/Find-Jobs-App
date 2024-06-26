6e1eb387843ad15052589e8ea83f77e077069f63
Murad Farhat <92052407+muradfarhat@users.noreply.github.com>
2024-06-26 13:18:18 +0400

    Login Fragment Update

--- app/src/main/java/com/example/sampleproject/login_fragment.kt
+++ app/src/main/java/com/example/sampleproject/login_fragment.kt
@@ -1,10 +1,18 @@
 package com.example.sampleproject
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+//import androidx.fragment.app.Fragment
+import android.app.Fragment
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.example.sampleproject.util.CommonUtils
+import com.example.sampleproject.views.MainActivity
+import com.example.sampleproject.views.activities.RecyclerViewActivity
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,7 +24,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [login_fragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class login_fragment : Fragment() {
+class LoginFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -27,14 +35,30 @@ class login_fragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_login_fragment, container, false)
+
+        val username = view.findViewById<EditText>(R.id.userName)
+        val password = view.findViewById<EditText>(R.id.password)
+        val loginBtn = view.findViewById<TextView>(R.id.loginbtn)
+
+        loginBtn.setOnClickListener {
+            if (username.text.isNotEmpty() && CommonUtils.isValidEmailFormat(username.text.toString()) && password.text.isNotEmpty()) {
+                val intent = Intent(activity, RecyclerViewActivity::class.java)
+                intent.putExtra("EXTRA_USERNAME", username.text.toString())
+                startActivity(intent)
+            } else {
+                Toast.makeText(activity, getString(R.string.invalid_email_or_password), Toast.LENGTH_SHORT).show()
+            }
+        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_fragment, container, false)
+        return view
     }
 
     companion object {
@@ -49,7 +73,7 @@ class login_fragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            login_fragment().apply {
+            LoginFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/example/sampleproject/views/MainActivity.kt
+++ app/src/main/java/com/example/sampleproject/views/MainActivity.kt
@@ -5,13 +5,16 @@ import android.content.res.Configuration
 import android.os.Bundle
 import android.view.WindowManager
 import android.widget.EditText
+import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sampleproject.R
+import com.example.sampleproject.LoginFragment
 import com.example.sampleproject.util.CommonUtils
 import com.example.sampleproject.views.activities.Item
 import com.example.sampleproject.views.activities.JobItemAdapter
@@ -23,21 +26,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val username = findViewById<EditText?>(R.id.userName)
-        val password = findViewById<EditText?>(R.id.password)
-        val loginBtn: TextView = findViewById(R.id.loginbtn)
-        var intent: Intent
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.loginFragmentContainer, LoginFragment())
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
 
-        loginBtn.setOnClickListener {
-                if(username.text.isNotEmpty() && CommonUtils.isValidEmailFormat(username.text.toString()) && password.text.isNotEmpty()) {
-                intent = Intent(this@MainActivity, RecyclerViewActivity::class.java)
-                intent.putExtra("EXTRA_USERNAME", username.text.toString())
-                startActivity(intent)
-            } else {
-                Toast.makeText(this@MainActivity,
-                    getString(R.string.invalid_email_or_password), Toast.LENGTH_SHORT).show()
-            }
-        }
+//        val username = findViewById<EditText?>(R.id.userName)
+//        val password = findViewById<EditText?>(R.id.password)
+//        val loginBtn: TextView = findViewById(R.id.loginbtn)
+//        var intent: Intent
+//
+//        loginBtn.setOnClickListener {
+//                if(username.text.isNotEmpty() && CommonUtils.isValidEmailFormat(username.text.toString()) && password.text.isNotEmpty()) {
+//                intent = Intent(this@MainActivity, RecyclerViewActivity::class.java)
+//                intent.putExtra("EXTRA_USERNAME", username.text.toString())
+//                startActivity(intent)
+//            } else {
+//                Toast.makeText(this@MainActivity,
+//                    getString(R.string.invalid_email_or_password), Toast.LENGTH_SHORT).show()
+//            }
+//        }
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
